e8259873f4b360b04a6bcec3cf11bf2caf695ba8
Witold Buszkiewicz <witold.buszkiewicz@gmail.com>
2018-10-19 13:04:42 +0200

    First commit

---
+++ przekaznik_czasowy.h
@@ -0,0 +1,14 @@
+/*
+ * przekaznik_czasowy.h
+ *
+ *  Created on: 17.10.2018
+ *      Author: witek
+ */
+
+#ifndef PRZEKAZNIK_CZASOWY_H_
+#define PRZEKAZNIK_CZASOWY_H_
+
+#define ENC_A 9
+#define ENC_B 10
+
+#endif /* PRZEKAZNIK_CZASOWY_H_ */
